195aa4403cd35b4d881e6d8e85d81934d441e7c1
luhu <luhu0105@gmail.com>
2020-11-13 19:35:20 +0800

    add substitution

--- new_user/models.py
+++ new_user/models.py
@@ -20,6 +20,7 @@ class BadmintonActivity(models.Model):
     is_alive = models.BooleanField('是否结束', default=False)
     is_full = models.BooleanField('是否订满', default=False)
     limit_count = models.IntegerField('人数限制', default=100)
+    activity_place = models.CharField('活动地点', max_length=100, default='')
 
     class Meta:
         verbose_name = '活动列表'
@@ -35,7 +36,7 @@ class BadmintonActivityDetails(models.Model):
     is_substitution = models.BooleanField(verbose_name='是否替补', default=False)
 
     class Meta:
-        unique_together = ('activity_number', 'join_weChat',)   # 多个字段联合唯一约束
+        unique_together = ('activity_number', 'join_weChat', 'is_substitution')   # 多个字段联合唯一约束
         verbose_name = '活动详情'
         verbose_name_plural = verbose_name
 
--- new_user/views.py
+++ new_user/views.py
@@ -99,29 +99,40 @@ def activityView(request, number):
     activityDetails = BadmintonActivityDetails.objects.filter(activity_number_id=int(number))
     join_dic = {}
     new_join_dic = {}
-    is_join = BadmintonActivityDetails.objects.filter(activity_number_id=int(number), join_weChat_id=request.user.id)
+    is_join = BadmintonActivityDetails.objects.filter(activity_number_id=int(number), join_weChat_id=request.user.id, is_substitution=False)
+    is_substitution = BadmintonActivityDetails.objects.filter(activity_number_id=int(number), join_weChat_id=request.user.id, is_substitution=True)
     is_full = BadmintonActivity.objects.filter(id=int(number)).values_list('is_full')[0][0]
     join = request.POST.get("join", '')
     cancel = request.POST.get("cancel", '')
     substitution = request.POST.get("substitution", '')
+    cancel_substitution = request.POST.get("cancel_substitution", '')
+
     for i in activityDetails:
         join_dic = dict(BadmintonActivityDetails.objects.filter(activity_number_id=int(number)).values_list('join_weChat', 'is_substitution'))
         for j in join_dic.keys():
             new_join_dic[MyUser.objects.get(id=j).weChat] = join_dic[j]
-            logger.info(new_join_dic)
-    if bool(is_join) and join == 'join':
-        txt = '您已成功报名，请勿重复报名'
-        logger.info(txt)
-    elif join == 'join':
-        join_person = BadmintonActivityDetails(join_weChat_id=request.user.id, activity_number_id=int(number))
-        join_person.save()
-        logger.info('报名成功')
-    elif cancel == 'cancel':
-        cancel_activity = BadmintonActivityDetails.objects.filter(activity_number_id=int(number), join_weChat_id=request.user.id).delete()
-        logger.info('取消报名成功')
-    elif substitution == 'substitution':
-        substitution_activity = BadmintonActivityDetails(join_weChat_id=request.user.id, activity_number_id=int(number), is_substitution=True)
-        logger.info('替补成功')
+            # logger.info(new_join_dic)
+    try:
+        if bool(is_join) and join == 'join':
+            txt = '您已成功报名，请勿重复报名'
+            logger.info(txt)
+        elif join == 'join':
+            join_person = BadmintonActivityDetails(join_weChat_id=request.user.id, activity_number_id=int(number))
+            join_person.save()
+            logger.info('报名成功')
+        elif cancel == 'cancel':
+            cancel_activity = BadmintonActivityDetails.objects.filter(activity_number_id=int(number), join_weChat_id=request.user.id).delete()
+            logger.info('取消报名成功')
+        elif substitution == 'substitution':
+            substitution_activity = BadmintonActivityDetails(join_weChat_id=request.user.id, activity_number_id=int(number),
+                                                             is_substitution=True)
+            substitution_activity.save()
+            logger.info('替补成功')
+        elif cancel_substitution == 'cancel_substitution':
+            cancel_activity = BadmintonActivityDetails.objects.filter(activity_number_id=int(number), join_weChat_id=request.user.id).delete()
+            logger.info('取消替补成功')
+    except Exception as e:
+        logger.info('报错了')
     context = {
         'activity': activityDetails,
         'user_info': new_join_dic
