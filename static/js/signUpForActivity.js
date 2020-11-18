25f3c87280591d108d1d6182db7514b418d0b971
luhu <luhu0105@gmail.com>
2020-11-18 11:21:09 +0800

    delete branch repeat

--- static/js/signUpForActivity.js
+++ static/js/signUpForActivity.js
@@ -5,7 +5,6 @@ $(document).ready(function(){
         let actionVal=$(signButton.children()[0]).val()
         let userName=$('#username').text()
         let info={"username":userName,"action":actionVal}
-<<<<<<< HEAD
         $.post(window.location.pathname,info,(result)=>{
             //result是string类型
             let index1=result.indexOf('<div class="pop">')
@@ -18,18 +17,6 @@ $(document).ready(function(){
             $(".pop p").text(text2)
                 $(".pop").fadeIn(200)
                 $(".mask").fadeIn(200)
-
-=======
-        $.post(window.location.pathname,info,()=>{
-
-            // $(".pop p").text(text)
-           // $(".pop p").text("报名成功")
-                $(".pop").fadeIn(200)
-                $(".mask").fadeIn(200)
-            console.log($(".pop p"))
-            let text=$(".pop p").text()
-            console.log(text)
->>>>>>> e36a91c2c77bcb7cb95a05f8139cff884a1b2660
         })
         $(".pop button").click(()=>{
             $(".pop").fadeOut(200)
